b67767539c14236511962a7f58b59c3c9471f05e
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-04-05 04:19:24 -0400

    Add files via upload

---
+++ for s in range,parenthesis,two dots.py
@@ -0,0 +1,18 @@
+
+#for i in range(iterations wanted):loop 
+#the square with using just 3 lines
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor
+
+alex= turtle.Turtle()
+alex.color("black")
+alex.pensize(5)
+
+for i in range(4): #first line
+    alex.forward(50) #second line
+    alex.left(90) #third line
+
+
+wn.mainloop()
