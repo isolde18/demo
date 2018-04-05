9f09860f567ca829a5c248ab42d61e7b8069dec1
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-04-05 04:53:57 -0400

    Add files via upload

---
+++ two_squares.py
@@ -0,0 +1,15 @@
+import turtle
+wn=turtle.Screen()
+wn.bgcolor("green")
+wn.title ("xyz")
+
+xyz=turtle.Turtle()
+xyz.color("hotpink")
+xyz.pensize(3)
+
+for i in range(10):
+    xyz.forward(50)
+    xyz.left(90)
+
+
+#wn.mainloop()
